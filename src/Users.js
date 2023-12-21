b5a4a4a37e8ed8c88aea58f5a81d5ca2d44fc7d5
Pasindu Rathnathilake <ikpirathnathilake@students.nsbm.ac.lk>
2023-12-21 13:06:41 +0530

    create tables and link them

--- src/Users.js
+++ src/Users.js
@@ -1,7 +1,31 @@
+import { Box } from "@mui/material";
 import UserForm from "./UserForm";
+import UsersTable from "./UsersTable";
+
+const users = [
+  {
+    id: 1,
+    name: "Prasad",
+  },
+  {
+    id: 2,
+    name: "Amrith",
+  },
+];
 
 const Users = () => {
-  return <UserForm />;
+  return (
+    <Box
+      sx={{
+        width: "calc(100% - 100px)",
+        margin: "auto",
+        marginTop: "100px",
+      }}
+    >
+      <UserForm />
+      <UsersTable rows={users} />
+    </Box>
+  );
 };
 
 export default Users;
